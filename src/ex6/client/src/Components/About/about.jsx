00ca2059a61bb2d6cd872a9882dfdc5460392766
vico958 <74486817+vico958@users.noreply.github.com>
2022-07-07 17:43:32 +0300

    Much less files

--- src/ex6/client/src/Components/About/about.jsx
+++ src/ex6/client/src/Components/About/about.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import profile_photo from '../Images/profile_photo.jpg';
-import styles from './about.module.css';
+    import React from 'react';
+    import profile_photo from '../Images/profile_photo.jpg';
+    import styles from './about.module.css';
 
-export const About = () => {
-    return(
-        <section>
-            <img src = {profile_photo} className = {styles.profile_photo}/>
-            <p className = {styles.text_about_page}>My name is Viktor dabush, I am 28 years old.</p>
-            <p className = {styles.text_about_page}>I have collection of Pokemon and Yu-gi-oh cards.</p>
-            <p className = {styles.text_about_page}>I used to own an online business where i helped to ebay sellers, this way i made over 200k from my bed.</p>
-    </section>
-   );
-}
+    export const About = () => {
+        return(
+            <section>
+                <img src = {profile_photo} className = {styles.profile_photo}/>
+                <p className = {styles.text_about_page}>My name is Viktor dabush, I am 28 years old.</p>
+                <p className = {styles.text_about_page}>I have collection of Pokemon and Yu-gi-oh cards.</p>
+                <p className = {styles.text_about_page}>I used to own an online business where i helped to ebay sellers, this way i made over 200k from my bed.</p>
+        </section>
+    );
+    }
--- src/ex6/client/src/Components/NavigationBar/navigationOnTheWeb.jsx
+++ src/ex6/client/src/Components/NavigationBar/navigationOnTheWeb.jsx
@@ -1,19 +1,19 @@
-import React from 'react';
-import { About } from '../About/about';
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import { TodoApp } from '../TodoApp/todoApp';
-import {Statistics} from '../Statistics/statistics';
+  import React from 'react';
+  import { About } from '../About/about';
+  import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+  import { TodoApp } from '../TodoApp/todoApp';
+  import {Statistics} from '../Statistics/statistics';
 
-export const NavigationOnTheWeb = () => {
-    return(
-        <section>
-    <Router>
-      <Routes>
-        <Route path='/' element={<TodoApp/>} />
-        <Route path='/about' element={<About/>} />
-        <Route path = '/statistics' element ={<Statistics/>}/>
-      </Routes>
-    </Router>
-    </section>
-   );
-}
+  export const NavigationOnTheWeb = () => {
+      return(
+          <section>
+      <Router>
+        <Routes>
+          <Route path='/' element={<TodoApp/>} />
+          <Route path='/about' element={<About/>} />
+          <Route path = '/statistics' element ={<Statistics/>}/>
+        </Routes>
+      </Router>
+      </section>
+    );
+  }
