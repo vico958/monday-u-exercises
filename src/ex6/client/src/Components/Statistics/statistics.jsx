ea340423e0b399ab16064f2016375290b9e6def8
vico958 <74486817+vico958@users.noreply.github.com>
2022-07-14 12:14:42 +0300

    now work again

--- src/ex6/client/src/Components/Statistics/statistics.jsx
+++ src/ex6/client/src/Components/Statistics/statistics.jsx
@@ -1,48 +1,61 @@
-    import React, { useState, useEffect } from 'react';
-    import styles from './statistics.module.css';
-    import { getItems } from '../apiRequestsLib';
-    import { Typography } from "@mui/material";
+import React, { useState, useEffect } from "react";
+import styles from "./statistics.module.css";
+import { getItems } from "../apiRequestsLib";
+import { Typography } from "@mui/material";
 
-    export const Statistics = () => {
-        const [todoList, setTodoList] = useState([]);
-        const [countFinishedTask, setCountFinishedTask] = useState(0);
+export const Statistics = () => {
+  const [todoList, setTodoList] = useState([]);
+  const [countFinishedTask, setCountFinishedTask] = useState(0);
 
-        useEffect(() => {
-            setingTodoList();
-            finishedTaskAmount();
-        }, [todoList])
+  useEffect(() => {
+    setingTodoList();
+    finishedTaskAmount();
+  }, [todoList]);
 
-        const setingTodoList = async () => {
-            setTodoList(await getItems());
-        }
+  const setingTodoList = async () => {
+    setTodoList(await getItems());
+  };
 
-        const finishedTaskAmount = () => {
-            let count = 0;
-            todoList.forEach(todo => {
-                if(todo.isCompleted){
-                    count++;
-                }
-            });
-            setCountFinishedTask(count);
-        }
-        
-        return(
-            <section>
-                <Typography variant = "h1" color = "purple">
-                Statistics
-                </Typography>
-                <p className = {styles.text_statistics_page}>You have {todoList.length} tasks.</p>
+  const finishedTaskAmount = () => {
+    let count = 0;
+    todoList.forEach((todo) => {
+      if (todo.isCompleted) {
+        count++;
+      }
+    });
+    setCountFinishedTask(count);
+  };
 
-                {countFinishedTask >= 1 ?
-                <p className = {styles.text_statistics_page}>You finished {countFinishedTask} of {todoList.length} task already, good job!!!.</p>
-                : <></>}
+  return (
+    <section>
+      <Typography variant="h1" color="purple">
+        Statistics
+      </Typography>
+      <p className={styles.text_statistics_page}>
+        You have {todoList.length} tasks.
+      </p>
 
-                {todoList.length >= 1 && countFinishedTask === 0 ?
-                <div>
-                    <p className = {styles.text_statistics_page}>You didnt finish any task until now.</p>
-                    <p className = {styles.text_statistics_page}>What are you waiting for? stop coding and start working!</p>
-                </div>
-                : <></>}
-        </section>
-    );
-    }
+      {countFinishedTask >= 1 ? (
+        <p className={styles.text_statistics_page}>
+          You finished {countFinishedTask} of {todoList.length} task already,
+          good job!!!.
+        </p>
+      ) : (
+        <></>
+      )}
+
+      {todoList.length >= 1 && countFinishedTask === 0 ? (
+        <div>
+          <p className={styles.text_statistics_page}>
+            You didnt finish any task until now.
+          </p>
+          <p className={styles.text_statistics_page}>
+            What are you waiting for? stop coding and start working!
+          </p>
+        </div>
+      ) : (
+        <></>
+      )}
+    </section>
+  );
+};
--- src/ex6/client/src/Components/TodoApp/todoApp.jsx
+++ src/ex6/client/src/Components/TodoApp/todoApp.jsx
@@ -1,14 +1,12 @@
-  import React from 'react';
-  import { Typography } from "@mui/material";
-  import { TodoContainer } from '../TodoContainer/TodoContainer/todoContainer';
+import React from "react";
+import { Typography } from "@mui/material";
+import { TodoContainer } from "../TodoContainer/TodoContainer/todoContainer";
 
-  export const TodoApp = () => {
-      return(
-          <section>
-              <Typography variant={"h1"}>
-                Todo App
-              </Typography>
-              <TodoContainer/>
-      </section>
-    );
-  }
+export const TodoApp = () => {
+  return (
+    <section>
+      <Typography variant={"h1"}>Todo App</Typography>
+      <TodoContainer />
+    </section>
+  );
+};
